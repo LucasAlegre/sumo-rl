3829832efa452e1b5752fdd3ac273d9dcbe5d57e
Lucas Alegre <lucasnale@gmail.com>
2018-12-14 15:47:21 -0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+REQUIRED = ['gym', 'numpy', 'pandas', 'matplotlib']
+
+setup(
+    name='sumo-rl',
+    version='0.1dev',
+    packages=['agents', 'environment', 'experiments', 'exploration'],
+    install_requires=REQUIRED,
+    author='LucasAlegre',
+    author_email='lucasnale@gmail.com',
+    description='Environments inheriting OpenAI Gym Env and RL algorithms to control Traffic Signal controllers on SUMO.'
+)
