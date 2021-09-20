d653445469d76b0ab70090e8ee22b096be4185bd
Lucas Alegre <lucasnale@gmail.com>
2021-09-19 23:45:48 -0300

    Close env after test

--- tests/pz_test.py
+++ tests/pz_test.py
@@ -2,10 +2,10 @@ from pettingzoo.test import api_test
 from sumo_rl import SumoEnvironmentPZ
 
 env = SumoEnvironmentPZ(net_file='nets/4x4-Lucas/4x4.net.xml',
-                                                    route_file='nets/4x4-Lucas/4x4c1c2c1c2.rou.xml',
-                                                    out_csv_name='outputs/4x4grid/a3c',
-                                                    use_gui=False,
-                                                    num_seconds=80000,
-                                                    max_depart_delay=0)
+                        route_file='nets/4x4-Lucas/4x4c1c2c1c2.rou.xml',
+                        out_csv_name='outputs/4x4grid/a3c',
+                        use_gui=False,
+                        num_seconds=80000)
 
 api_test(env)
+env.close()
