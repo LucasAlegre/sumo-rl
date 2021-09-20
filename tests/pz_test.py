ae21374912718f560f7ca22bf9ef8c0bf2885a29
Lucas Alegre <lucasnale@gmail.com>
2021-09-19 23:59:23 -0300

    Fix call to pytest

--- tests/pz_test.py
+++ tests/pz_test.py
@@ -1,11 +1,17 @@
 from pettingzoo.test import api_test
 from sumo_rl import SumoEnvironmentPZ
 
-env = SumoEnvironmentPZ(net_file='nets/4x4-Lucas/4x4.net.xml',
-                        route_file='nets/4x4-Lucas/4x4c1c2c1c2.rou.xml',
-                        out_csv_name='outputs/4x4grid/a3c',
-                        use_gui=False,
-                        num_seconds=80000)
 
-api_test(env)
-env.close()
+def test_api():
+    env = SumoEnvironmentPZ(net_file='nets/4x4-Lucas/4x4.net.xml',
+                            route_file='nets/4x4-Lucas/4x4c1c2c1c2.rou.xml',
+                            out_csv_name='outputs/4x4grid/a3c',
+                            use_gui=False,
+                            num_seconds=80000)
+
+    api_test(env)
+    env.close()
+
+
+if __name__ == '__main__':
+    test_api()
