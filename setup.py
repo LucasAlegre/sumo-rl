d1ddd9744546ddf080a1eb783daefb5fb2c5ce4e
Lucas Alegre <lucasnale@gmail.com>
2021-09-26 22:31:11 -0300

    Add PIL dependency

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-REQUIRED = ['gym', 'numpy', 'pandas', 'PIL']
+REQUIRED = ['gym', 'numpy', 'pandas', 'pillow']
 
 extras = {
     "pettingzoo": ["pettingzoo"],
