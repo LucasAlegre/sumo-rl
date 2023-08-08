b0e1d2522b94e9473091fbc4f54a8bfc908a2711
船长 <pxn@cychina.cn>
2023-08-08 10:18:57 +0800

    第一阶段测试完成，详情参见NOTES.md

--- experiments/sb3_grid4x4.py
+++ experiments/sb3_grid4x4.py
@@ -14,7 +14,6 @@ from tqdm import trange
 
 import sumo_rl
 
-
 if __name__ == "__main__":
     RESOLUTION = (3200, 1800)
 
@@ -80,3 +79,24 @@ if __name__ == "__main__":
     print("All done, cleaning up")
     shutil.rmtree("temp")
     env.close()
+
+# 测试说明：2023-07-17--2023-08-08
+# 1. 本次测试时间为2023-07-17--2023-08-08，在MacOS M1 Ventura 13.4.1 和 Ubuntu 22.04 LTS (GPU 4090)上测试通过。
+# 2. 各主要组件的版本如下：
+#   - python 3.10.0
+#   - pytorch 2.0.1
+#   - stable-baselines3 2.0.0a13
+#   - pettingzoo 1.23.1
+#   - supersuit 3.9.0
+#   - sumo-rl 1.4.3
+#   - sumo 1.18.0
+#   - traci 1.18.0
+#   - gymnasium 0.28.1
+#   - gym 0.26.2
+#   - ray 2.5.0
+#   - ray[rllib] 2.5.0
+# 3. 必须使用gui模式，否则无法启动服务，客户端无法连接，导致connection refused错误
+# 4. 关闭LIBSUMO_AS_TRACI环境变量，使用client-server模式，启动sumo服务,使用traci客户端连接，采用服务间的通信，而非进程间通信。
+# 5. experiments里的所有案例都运行成功，画图成功。
+# 6. 所有的测试结果都没有分析，所有的超参数都没有进行优化。
+# 7. 后期计划
