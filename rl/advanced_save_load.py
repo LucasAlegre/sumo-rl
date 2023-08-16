9f0c4c9bced0b554bdf5e1ab05911d6928c4fc59
sitexa <sitexa@gmail.com>
2023-08-16 14:52:26 +0800

    save folder

--- rl/advanced_save_load.py
+++ rl/advanced_save_load.py
@@ -9,17 +9,17 @@ model = SAC("MlpPolicy", "Pendulum-v1", verbose=1, learning_rate=1e-3)
 model.learn(total_timesteps=6000)
 
 # save the model
-model.save("sac_pendulum")
+model.save("./model/sac_pendulum")
 
 # the saved model does not contain the replay buffer
-loaded_model = SAC.load("sac_pendulum")
+loaded_model = SAC.load("./model/sac_pendulum")
 print(f"The loaded_model has {loaded_model.replay_buffer.size()} transitions in its buffer")
 
 # now save the replay buffer too
-model.save_replay_buffer("sac_replay_buffer")
+model.save_replay_buffer("./model/sac_replay_buffer")
 
 # load it into the loaded_model
-loaded_model.load_replay_buffer("sac_replay_buffer")
+loaded_model.load_replay_buffer("./model/sac_replay_buffer")
 
 # now the loaded replay is not empty anymore
 print(f"The loaded_model has {loaded_model.replay_buffer.size()} transitions in its buffer")
@@ -28,7 +28,7 @@ print(f"The loaded_model has {loaded_model.replay_buffer.size()} transitions in 
 # Note: if you don't save the complete model with `model.save()`
 # you cannot continue training afterward
 policy = model.policy
-policy.save("sac_policy_pendulum")
+policy.save("./model/sac_policy_pendulum")
 
 # Retrieve the environment
 env = model.get_env()
@@ -39,7 +39,7 @@ mean_reward, std_reward = evaluate_policy(policy, env, n_eval_episodes=10, deter
 print(f"mean_reward={mean_reward:.2f} +/- {std_reward}")
 
 # Load the policy independently from the model
-saved_policy = MlpPolicy.load("sac_policy_pendulum")
+saved_policy = MlpPolicy.load("./model/sac_policy_pendulum")
 
 # Evaluate the loaded policy
 mean_reward, std_reward = evaluate_policy(saved_policy, env, n_eval_episodes=10, deterministic=True)
