f93be8badcf877edd68430b03da632589b2771e8
sitexa <sitexa@gmail.com>
2024-06-27 19:41:35 +0800

    file exist?

--- mynets/t2.py
+++ mynets/t2.py
@@ -1,4 +1,5 @@
 import sys
+from pathlib import Path
 
 sys.path.append('..')
 
@@ -6,3 +7,10 @@ import mysumo.envs
 from mysumo.envs.sumo_env import SumoEnv
 
 print("hello world")
+
+model_file = "model/my-intersection-modelPPO.zip"
+
+file_path = Path(model_file)
+if file_path.exists():
+    print("load model=====加载训练模型==在原来基础上训练")
+    # model.load(model_file)
