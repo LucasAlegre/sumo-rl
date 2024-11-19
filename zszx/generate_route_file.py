5ed170def4fbca311a1261458ab8dce836a31c1a
sitexa <sitexa@gmail.com>
2024-11-19 18:51:59 +0800

    ready to run

---
+++ zszx/generate_route_file.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+# 假设你的历史数据已经存储在一个CSV文件中
+data = pd.read_csv('./zszx/output_data.txt', sep='\t', header=None, names=['timestamp', 'direction', 'plate'])
+
+with open("./zszx/zszx.rou.xml", "w") as routes:
+    print(
+        """<routes>
+    <vType accel="1.0" decel="4.5" id="standard_car" length="5.0" minGap="2.5" maxSpeed="25" sigma="0.5" />
+    <route id="WN" edges="w_t t_n"/>
+    <route id="WE" edges="w_t t_e"/>
+    <route id="WS" edges="w_t t_s"/>
+    
+    <route id="NW" edges="n_t t_w"/>
+    <route id="NE" edges="n_t t_e"/>
+    <route id="NS" edges="n_t t_s"/>
+    
+    <route id="EW" edges="e_t t_w"/>
+    <route id="EN" edges="e_t t_n"/>
+    <route id="ES" edges="e_t t_s"/>
+    
+    <route id="SW" edges="s_t t_w"/>
+    <route id="SN" edges="s_t t_n"/>
+    <route id="SE" edges="s_t t_e"/>""",
+        file=routes,
+    )
+
+    start_time = 0
+    veh_counter = 0
+    # 假设每条数据包含时间戳、方向和车牌
+    for _, row in data.iterrows():
+        direction = row['direction']  # 可以根据方向设置车道等信息
+        vehicle_id = row['plate']  # 车牌号
+        timestamp = pd.to_datetime(row['timestamp'])  # 时间戳
+        vehicle_id = vehicle_id + "_" + str(veh_counter)
+        veh_counter += 1
+
+        if start_time == 0:
+            start_time = timestamp
+
+        depart_time = (timestamp - start_time).total_seconds()
+
+        print(
+            '    <vehicle id="%s" type="standard_car" route="%s" depart="%s" departLane="random" departSpeed="10" />'
+            % (vehicle_id, direction, depart_time),
+            file=routes,
+        )
+
+    print("</routes>", file=routes)
