69342d52f248f39e8bf882612d04cd6a689f254f
Taha Pamukcu <160586944+MoonGlow22@users.noreply.github.com>
2025-04-26 13:42:33 +0300

    path fixs in experiments folder

--- experiments/dqn_big-intersection.py
+++ experiments/dqn_big-intersection.py
@@ -17,10 +17,10 @@ from sumo_rl import SumoEnvironment
 
 
 env = SumoEnvironment(
-    net_file="sumo_rl/nets/big-intersection/big-intersection.net.xml",
+    net_file="../sumo_rl/nets/big-intersection/big-intersection.net.xml",
     single_agent=True,
-    route_file="sumo_rl/nets/big-intersection/routes.rou.xml",
-    out_csv_name="outputs/big-intersection/dqn",
+    route_file="../sumo_rl/nets/big-intersection/routes.rou.xml",
+    out_csv_name="../outputs/big-intersection/dqn",
     use_gui=True,
     num_seconds=5400,
     yellow_time=4,
